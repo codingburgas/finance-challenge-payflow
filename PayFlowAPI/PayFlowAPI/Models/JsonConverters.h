ce88ea7ae36d49deddaa19474a9047dc2b5d7c59
Polina Dineva <PDDineva22@codingburgas.bg>
2024-10-20 16:16:05 +0300

    Add json conversion

---
+++ PayFlowAPI/PayFlowAPI/Models/JsonConverters.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "ResponseModels\PaymentResponse.h"
+#include <nlohmann/json.hpp>
+
+void to_json(nlohmann::json& j, const PaymentResponse& e) {
+	j = nlohmann::json{
+		{"id", e.id},
+		{"userId",e.userId},
+		{"paymentTo", e.paymentTo},
+		{"paymentDate", e.paymentDate},
+		{"amount", e.amount},
+		{"billingAddress", e.billingAddress},
+		{"returnedAmount", e.returnedAmount}
+	};
+}
+
+void from_json(const nlohmann::json& j, PaymentResponse& e) {
+	e.id = j["id"];
+	e.userId = j["userId"];
+	e.paymentTo = j["paymentTo"];
+	e.paymentDate = j["paymentDate"];
+	e.amount = j["amount"];
+	e.billingAddress = j["billingAddress"];
+	e.returnedAmount = j["returnedAmount"];
+}
---
+++ PayFlowAPI/PayFlowAPI/Models/ResponseModels/PaymentResponse.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "..\EntityModels\Payment.h"
+class PaymentResponse
+{
+public:
+	int id;
+	int userId;
+	std::string paymentTo;
+	std::string paymentDate;
+	double amount;
+	std::string billingAddress;
+	int returnedAmount;
+
+	PaymentResponse(Payment p) : id(p.id), userId(p.userId), paymentTo(p.paymentTo), amount(p.amount), billingAddress(p.billingAddress)
+	{
+		paymentDate = 
+			std::to_string(p.paymentDate.year) + "-" +
+			std::to_string(p.paymentDate.month) + "-" +
+			std::to_string(p.paymentDate.day) + " " +
+			std::to_string(p.paymentDate.hour) + ":" +
+			std::to_string(p.paymentDate.min) + ":" +
+			std::to_string(p.paymentDate.sec) + ":";
+
+		if (p.returnedAmount == "NULL")
+		{
+			returnedAmount = -1;
+		}
+		else
+		{
+			returnedAmount = stoi(p.returnedAmount);
+		}
+	}
+
+	PaymentResponse(int id, int userId, std::string paymentTo, std::string paymentDate, double amount, std::string billingAddress, int returnedAmount) :
+		id(id), userId(userId), paymentTo(paymentTo), paymentDate(paymentDate), amount(amount), billingAddress(billingAddress), returnedAmount(returnedAmount) {};
+};
