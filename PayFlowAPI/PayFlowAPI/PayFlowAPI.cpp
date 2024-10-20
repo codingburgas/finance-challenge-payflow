8863d8fd27a987890a2200082e2a53b725fee836
Polina Dineva <PDDineva22@codingburgas.bg>
2024-10-20 13:49:34 +0300

    Add project for API

---
+++ PayFlowAPI/PayFlowAPI/PayFlowAPI.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <nanodbc/nanodbc.h>
+#include <optional>
+#include <crow.h>
+#include <nlohmann/json.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
