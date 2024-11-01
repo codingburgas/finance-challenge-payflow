2402821309d6d1b5464721dac02ab3d13486e4ed
Polina Dineva <PDDineva22@codingburgas.bg>
2024-11-01 19:51:14 +0200

    Add budgets js add and get functionality

--- PayFlow/src/js/budget.js
+++ PayFlow/src/js/budget.js
@@ -14,7 +14,19 @@ initData();
 function initData()
 {
     getUserData();
+    getBudgets();
 }
+
+document.getElementById('addBudgetForm').addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formJSON = Object.fromEntries(formData.entries());
+    formJSON.userId = parseInt(localStorage.getItem('userId'));
+    formJSON.id = parseInt(-1);
+    formJSON.amount = parseInt(formJSON.amount);
+    addBudget(formJSON);
+});
+
 
 function getUserData()
 {
@@ -25,6 +37,79 @@ function getUserData()
             if(response.data != null)
             {
                 document.getElementById('welcome-name').innerText = ` ${response.data.userName}`
+            }
+        }
+        else
+        {
+            console.log(error);
+            alert("Unauthorised");
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+        alert("Fatal error");
+    });
+}
+
+function getBudgets()
+{
+     axios.get(apiURL + `budget/getByUser/${localStorage.getItem('userId')}`)
+     .then(function (response) {
+        if(response.status == 200)
+         {
+            
+            const oldEarnings = document.querySelectorAll(".budget-item");
+            oldEarnings.forEach(oldEarning => oldEarning.remove());
+             if(response.data != null)
+             {
+                 console.log(response.data);
+
+                 let budgets = response.data;
+    
+                 for(let i = 0;i<budgets.length;i++)
+                 {
+                     document.getElementById('budgets').innerHTML+=`
+                         <div class="budget-item">
+                            <div class="budget-info">
+                                <div class="budget-icon"></div>
+                                <span>${budgets[i].expenseType}</span>
+                            </div>
+                            <span>${budgets[i].amount} $</span>
+                        </div>
+                     `;
+                 }
+             }
+         }
+         else
+         {
+             console.log(error);
+             alert("Unauthorised");
+        }
+     })
+     .catch(function (error) {
+         console.log(error);
+         alert("Fatal error");
+     });
+}
+
+function addBudget(body)
+{
+    axios.post(apiURL + 'budget/add', body)
+    .then(function (response) {
+        if(response.status == 200 || response.status == 204)
+        {
+            if(response != null)
+            {
+                console.log(response.data);
+                if(response.data.userId != -1)
+                {
+                    document.getElementById('successMessage').innerText;
+                }
+                else
+                {
+                    alert("Unauthorised");
+                }
+                getBudgets();
             }
         }
         else
